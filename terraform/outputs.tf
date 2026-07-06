9db8d8b28242fd4af66dda0f4bacfee2764702a9
Nikos Sklikas <nikos.sklikas@canonical.com>
2026-07-06 17:35:00 +0300

    chore: add terraform module

---
+++ terraform/outputs.tf
@@ -0,0 +1,32 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "The Juju application name"
+  value       = juju_application.application.name
+}
+
+output "application" {
+  description = "The deployed juju_application resource"
+  value       = juju_application.application
+}
+
+output "requires" {
+  description = "The Juju integrations that the charm requires"
+  value = {
+    logging               = "logging"
+    tracing               = "tracing"
+    authentik-server-info = "authentik-server-info"
+    ingress               = "ingress"
+    ldaps-ingress         = "ldaps-ingress"
+  }
+}
+
+output "provides" {
+  description = "The Juju integrations that the charm provides"
+  value = {
+    metrics-endpoint  = "metrics-endpoint"
+    grafana-dashboard = "grafana-dashboard"
+    ldap              = "ldap"
+  }
+}
